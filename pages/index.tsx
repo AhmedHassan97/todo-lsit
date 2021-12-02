39fe7f378eab161dc107e0111913f396663f50b1
AhmedHassan97 <ahahmed202025@gmail.com>
2021-12-02 20:22:44 +0200

    feat: add signup page

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import Signup from "../app/components/signup";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Signup />
+    </div>
+  );
+};
+
+export default Home;
