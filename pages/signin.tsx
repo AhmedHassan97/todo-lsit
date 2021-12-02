bdb5176ce8cccf21ee3ffd30f10b9b1ee3bce03b
AhmedHassan97 <ahahmed202025@gmail.com>
2021-12-02 20:23:03 +0200

    feat: add signin page

---
+++ pages/signin.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import SignIn from "../app/components/signin";
+
+const SignInPage: NextPage = () => {
+  return (
+    <div>
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignInPage;
